eac07ea7b23c220b1fe2073c379683936126c2e3
Lavanya P <lavanya.palaparthi@gmail.com>
2018-07-03 12:15:17 +0000

    - View jobs module changes

--- client/web/src/routes/dashboard.jsx
+++ client/web/src/routes/dashboard.jsx
@@ -6,8 +6,15 @@ import Icons from "../views/Icons/Icons";
 import Maps from "../views/Maps/Maps";
 import Notifications from "../views/Notifications/Notifications";
 import SignIn from "../views/SignIn/SignIn";
+import ViewJobs from "../views/ViewJobs/ViewJobs";
 
 const dashboardRoutes = [
+  {
+    path: "/ViewJobs",
+    name: "View Jobs",
+    icon: "pe-7s-search",
+    component: ViewJobs
+  },
   {
     path: "/dashboard",
     name: "Dashboard",
@@ -45,7 +52,8 @@ const dashboardRoutes = [
     icon: "pe-7s-bell",
     component: SignIn
   },
-  { redirect: true, path: "/", to: "/dashboard", name: "Dashboard" }
+  //default to view jobs
+  { redirect: true, path: "/", to: "/ViewJobs", name: "ViewJobs" }
 ];
 
 export default dashboardRoutes;
