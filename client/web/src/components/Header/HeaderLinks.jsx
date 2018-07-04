22a575e1d66c7e999204b9f96d64d354d094b388
react-warriors <react-warriors@users.noreply.github.com>
2018-07-04 07:41:07 +0000

    Removed unwanted fields

--- client/web/src/components/Header/HeaderLinks.jsx
+++ client/web/src/components/Header/HeaderLinks.jsx
@@ -35,25 +35,9 @@ class HeaderLinks extends Component {
             <p className="hidden-lg hidden-md">Search</p>
           </NavItem>
         </Nav>
-        <Nav pullRight>
-          <NavItem eventKey={1} href="#">
-            Account
-          </NavItem>
-          <NavDropdown
-            eventKey={2}
-            title="Dropdown"
-            id="basic-nav-dropdown-right"
-          >
-            <MenuItem eventKey={2.1}>Action</MenuItem>
-            <MenuItem eventKey={2.2}>Another action</MenuItem>
-            <MenuItem eventKey={2.3}>Something</MenuItem>
-            <MenuItem eventKey={2.4}>Another action</MenuItem>
-            <MenuItem eventKey={2.5}>Something</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={2.5}>Separated link</MenuItem>
-          </NavDropdown>
+        <Nav pullRight>          
           <NavItem eventKey={3} href="#">
-            Log out
+            Sign out
           </NavItem>
         </Nav>
       </div>
